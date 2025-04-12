24ea3a561ba5c367d21a97de8b84638857c5b5b6
InternetAstronaut <87543901+noobpen@users.noreply.github.com>
2025-04-12 18:08:40 -0300

    Final

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   return {
+    pathPrefix: "/website",
     dir: {
       input: ".",         // your input folder
       includes: "_includes", // layout files go here

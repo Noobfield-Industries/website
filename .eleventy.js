b3adec7cc43e9a63c59c4e0c2e32f7a2f03943b1
InternetAstronaut <87543901+noobpen@users.noreply.github.com>
2025-04-13 18:19:09 -0300

    Removed changes in #24ea3a561ba5c367d21a97de8b84638857c5b5b6

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   return {
-    pathPrefix: "/website",
+    // pathPrefix: "/website", // No need for this.
     dir: {
       input: ".",         // your input folder
       includes: "_includes", // layout files go here
